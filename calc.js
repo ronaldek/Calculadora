738c18f2cb39d8e568a27026ef155821b1040707
Ronald Guedes <ronald.gds13@gmail.com>
2022-06-07 20:36:06 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,48 @@
+
+function insert(num)
+{
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
+function clean()
+{
+    document.getElementById('resultado').innerHTML = "";
+}
+function back()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+function calcular()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado)
+    {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else
+    {
+        document.getElementById('resultado').innerHTML = ""
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var btn = document.getElementById('res').innerHTML;
+btn.addEventListener('click', function() {
+    insert('num')
+}, false)*/
